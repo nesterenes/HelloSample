189a68e30d9abd912f05e4a7731059a9c90277b1
Neil Shaffer <neil.shaffer@spectrumhealth.org>
2015-09-07 09:45:39 -0400

    Console, API, and Unit Test project creation.

---
+++ HelloSample/HelloSample.Api/Utility/WorldDataHelper.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="WorldDataHelper.cs" company="1UpApplications">
+//  Copyright (c) 2015 All Rights Reserved
+//  <author>Neil Shaffer</author>
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace HelloSample.Api.Utility
+{
+    /// <summary>
+    /// World data helper
+    /// </summary>
+    public class WorldDataHelper
+    {
+        /// <summary>
+        /// Gets the hello world.
+        /// </summary>
+        /// <returns>hello world</returns>
+        public static string GetHelloWorld()
+        {
+            return "Hello World";
+        }
+
+        /// <summary>
+        /// Gets the worlds.
+        /// </summary>
+        /// <returns>Array of Worlds</returns>
+        public static string[] GetWorlds()
+        {
+            return new string[] { "Mars", "Earth", "Pluto" };
+        }
+    }
+}
---
+++ HelloSample/HelloSample.Tests/API-Utility/DataAccessHelperTest.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DataAccessHelperTest.cs" company="1UpApplications">
+//  Copyright (c) 2015 All Rights Reserved
+//  <author>Neil Shaffer</author>
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace HelloSample.Tests.Controllers
+{
+    using HelloSample.Api.Utility;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// World Controller Tests
+    /// </summary>
+    [TestClass]
+    public class DataAccessHelperTest
+    {
+        /// <summary>
+        /// Get method test
+        /// </summary>
+        [TestMethod]
+        public void GetConnectionString_Test()
+        {
+            // Act
+            var result = DataAccessHelper.GetConnectionString();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
